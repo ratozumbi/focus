619d6f662e4c62a02db949b4f100d30ffed52124
RatoZumbi <brunowillianandrade@gmail.com>
2017-07-23 06:01:09 -0300

    Resolucao variavel
    Tratamento para que tudo escale corretamente. Falta tratar a aparicao das bolhas

--- Focus/Assets/Resources/Scripts/Bounce.cs
+++ Focus/Assets/Resources/Scripts/Bounce.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 public class Bounce : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
 
-	private Image bixo;
+	private Image bixoImage;
 	private RawImage chess;
 	private Vector3 inputVector;
 
@@ -25,15 +25,21 @@ public class Bounce : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDo
 	private bool canMove = true;
 	private Vector2 velocity = new Vector2(1,1);
     private Canvas canvas;
+    private Camera camera;
+
 
     // Use this for initialization
     void Start () {
 
 		velocity *= speed;
-        bixo = GameObject.Find("bixoFocus").GetComponent<Image>();
+        bixoImage = GameObject.Find("bixoFocus").GetComponent<Image>();
         chess = GameObject.Find ("chess").GetComponent<RawImage>();
-	
-	}
+
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -56,11 +62,15 @@ public class Bounce : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDo
 
     public virtual void OnPointerUp(PointerEventData ped){
 		canMove = true;
-		//velocity = Vector2.Reflect (velocity.normalized * speed, velocity.normalized);//
+        //velocity = Vector2.Reflect (velocity.normalized * speed, velocity.normalized);//
 
-		float y = GameObject.Find ("bixoFocus").transform.localPosition.y;
-		if (y < -900 && touch1time == true) {
-			bixo.enabled = false;
+        GameObject barra = GameObject.Find("qtdFoco");
+        GameObject bixoObj = GameObject.Find ("bixoFocus");
+        float dist = Vector2.Distance(barra.transform.position, bixoObj.transform.position);
+        Debug.Log(dist);
+
+        if (dist < 1 && touch1time == true) {
+			bixoImage.enabled = false;
 			touch1time = false;
 
 			GameObject.Find ("HandleEQ").GetComponent<Image>().enabled = true;
@@ -71,10 +81,12 @@ public class Bounce : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDo
 	}
 
     public virtual void OnDrag(PointerEventData ped){
-        transform.position = ped.position;
+        transform.localPosition = new Vector3((ped.position.x / canvas.scaleFactor) - ((camera.pixelWidth / 2) / canvas.scaleFactor), 
+            (ped.position.y / canvas.scaleFactor) - ((camera.pixelHeight/2) / canvas.scaleFactor));
+        //transform.position = ped.position;
 	}
 
-	void move()
+void move()
     {
 
         transform.Translate(velocity.x * Time.deltaTime, velocity.y * Time.deltaTime, 0);
