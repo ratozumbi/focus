02a4694b09cf1057f76eadc919c651cf56dcee3a
RatoZumbi <brunowillianandrade@gmail.com>
2017-08-02 17:50:26 -0300

    wip particulas; del import assets;

--- Focus/Assets/Resources/Scripts/1-1/Fosforo.cs
+++ Focus/Assets/Resources/Scripts/1-1/Fosforo.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Fosforo : MonoBehaviour {
+    public float F_Pos = 0.1f;
+    public float C_Pos = 0.36f;
+    public float U1_Pos = 0.55f;
+    public float U2_Pos = 0.71f;
+    public float S_Pos = 0.83f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float []slidePos = 
+
+    private GameObject F;
+    private GameObject O;
+    private GameObject C;
+    private GameObject U1;
+    private GameObject U2;
+    private GameObject S;
+
+    private GameObject barraEQ;
+    private Slider slider;
+    // Use this for initialization
+    void Start () {
+
+        F = GameObject.Find("F");
+        O = GameObject.Find("O");
+        C = GameObject.Find("C");
+        U1 = GameObject.Find("U1");
+        U2 = GameObject.Find("U2");
+        S = GameObject.Find("S");
+
+        barraEQ = GameObject.Find("equilibrio");
+        slider = barraEQ.GetComponent<Slider>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (slider.value > F_Pos - 0.01 && slider.value < F_Pos + 0.01)
+        {
+
+        }
+
 	
 	}
 }
--- Focus/Assets/Resources/Scripts/1-1/IntroGC.cs
+++ Focus/Assets/Resources/Scripts/1-1/IntroGC.cs
@@ -10,12 +10,12 @@ public class IntroGC : MonoBehaviour {
     private int currEvtFase = 0;
 
     private float timer = 0;
-    private Image img;
+    private Image logoImg;
     
     // Use this for initialization
     void Start () {
         logo = GameObject.Find("logo");
-        img = logo.GetComponent<Image>();
+        logoImg = logo.GetComponent<Image>();
 
         timer = Time.time;
     }
@@ -29,8 +29,9 @@ public class IntroGC : MonoBehaviour {
             case 0:
                 if (Time.time - timer > 3f)
                 {
-                    img.color = new Color(255, 255, 255, img.color.a + 0.01f);
-                    if (img.color.a > 0.999)
+                    //surge logo empresa
+                    logoImg.color = new Color(255, 255, 255, logoImg.color.a + 0.01f);
+                    if (logoImg.color.a > 0.999)
                     {
                         timer = Time.time;
                         currEvtFase = 1;
@@ -38,42 +39,65 @@ public class IntroGC : MonoBehaviour {
                 }
                 break;
             case 1:
-                Debug.Log("stage 1");
+                //some logo empresa
                 if (Time.time - timer > 3f)
                 {
-                    img.color = new Color(255, 255, 255, img.color.a - 0.01f);
-                    if (img.color.a < 0.001)
+                    logoImg.color = new Color(255, 255, 255, logoImg.color.a - 0.01f);
+                    if (logoImg.color.a < 0.001)
                     {
                         currEvtFase = 2;
+                        timer = Time.time;
                         logo.SetActive(false);
                     }
-                        
+
                 }
                 break;
+
+            case 2:
+                //surge tilt e barra
+                if (Time.time - timer > 3f)
+                {
+                    Image[] vecBarra = new Image[8];
+                    vecBarra[0] = GameObject.Find("HandleEQ").GetComponent<Image>();
+                    vecBarra[1] = GameObject.Find("eqBackground").GetComponent<Image>();
+                    vecBarra[2] = GameObject.Find("trigF").GetComponent<Image>();
+                    vecBarra[3] = GameObject.Find("trigC").GetComponent<Image>();
+                    vecBarra[4] = GameObject.Find("trigU1").GetComponent<Image>();
+                    vecBarra[5] = GameObject.Find("trigU2").GetComponent<Image>();
+                    vecBarra[6] = GameObject.Find("trigS").GetComponent<Image>();
+                    vecBarra[7] = GameObject.Find("tilt").GetComponent<Image>(); 
+
+                    foreach (Image img in vecBarra)
+                    {
+                        img.color = new Color(255, 255, 255, img.color.a + 0.01f);
+                        if (img.color.a > 0.999)
+                        {
+                            currEvtFase = 3;
+                            timer = Time.time;
+                        }
+
+                    }
+
+                }
+                break;
+
+            case 3:
+                //some tilt
+                if (Time.time - timer > 3f)
+                {
+                    logoImg.color = new Color(255, 255, 255, logoImg.color.a - 0.01f);
+                    if (logoImg.color.a < 0.001)
+                    {
+                        currEvtFase = 2;
+                        timer = Time.time;
+                        logo.SetActive(false);
+                    }
+
+                }
+                break;
+
         }
 	
 	}
 
-    private void loop1()
-    {
-
-        Color col = img.color;
-        col.a = 255;
-
-        if (Time.time > 2f && Time.time < 3f)
-        {
-            Color.Lerp(img.color, col, Time.deltaTime);
-        }
-        else if (Time.time > 3f)
-        {
-            col.a -= 1;
-            if (col.a == 0)
-            {
-                currEvtFase = 1;
-            }
-        }
-
-        img.color = col;
-
-    }
 }
