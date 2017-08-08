812071fd3e4bd35e2971856f75752a3a280b4c1b
RatoZumbi <brunowillianandrade@gmail.com>
2017-08-08 19:35:25 -0300

    wip intro

--- Focus/Assets/Resources/Scripts/1-1/Fosforo.cs
+++ Focus/Assets/Resources/Scripts/1-1/Fosforo.cs
@@ -3,7 +3,8 @@ using UnityEngine.UI;
 using System.Collections;
 using System;
 
-public class Fosforo : MonoBehaviour {
+public class Fosforo : MonoBehaviour
+{
     public static float F_Pos = 0.1f;
     public static float C_Pos = 0.36f;
     public static float U1_Pos = 0.55f;
@@ -12,7 +13,7 @@ public class Fosforo : MonoBehaviour {
 
     public int ativacao = 3;
 
-    public enum Letras : int {F,C,U1,U2,S};
+    public enum Letras : int { F, C, U1, U2, S };
     public Letras letra;
 
     private float[] slidePos = new float[] { F_Pos, C_Pos, U1_Pos, U2_Pos, S_Pos };
@@ -22,13 +23,18 @@ public class Fosforo : MonoBehaviour {
     private GameObject myLetra;
     private GameObject mySpark;
 
+    private ParticleSystem ps;
+
     private GameObject barraEQ;
     private Slider slider;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         myLetra = transform.Find("letra").gameObject;
         mySpark = transform.Find("letra/spark").gameObject;
+
+
 
         barraEQ = GameObject.Find("equilibrio");
         slider = barraEQ.GetComponent<Slider>();
@@ -36,34 +42,41 @@ public class Fosforo : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
-            if (slider.value > slidePos[(int)letra] - 0.01 && slider.value < slidePos[(int)letra] + 0.01)
+        if (slider.value > slidePos[(int)letra] - 0.01 && slider.value < slidePos[(int)letra] + 0.01)
+        {
+            if (slideActive == false)
             {
-                if(slideActive == false)
+                slideActive = true;
+
+                ParticleSystem ps = mySpark.GetComponent<ParticleSystem>();
+                ParticleSystem.EmitParams emitOverride = new ParticleSystem.EmitParams();
+                emitOverride.startLifetime = 10f;
+                ps.Emit(emitOverride, 20);
+
+                //mySpark.SetActive(true);// << melhorar isso aqui
+
+                slideCount++;
+
+                if (slideCount > ativacao)
                 {
-                    slideActive = true;
-                    mySpark.SetActive(true);// << melhorar isso aqui
-
-                    slideCount++;
-
-                    if (slideCount > ativacao)
-                    {
                     Image myImg = this.GetComponent<Image>();
                     Image imgLetra = myLetra.GetComponent<Image>();
 
-                    mySpark.SetActive(false);
+                    //mySpark.SetActive(false);
                     imgLetra.enabled = true;
                     myImg.enabled = false;
-                    
-                    }
+
                 }
             }
-            else
-            {
-                slideActive = false;
-                mySpark.SetActive(false);
-            }   
-	
-	}
+        }
+        else
+        {
+            slideActive = false;
+            //mySpark.SetActive(false);
+        }
+
+    }
 }
