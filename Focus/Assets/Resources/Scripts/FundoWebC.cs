2504efa5fb024502f5f17350e3d9fd9f0d0cea86
Ruilan Berg <rui_berg@hotmail.com>
2017-08-13 14:27:59 -0300

    add webcam texture in background init

--- Focus/Assets/Resources/Scripts/FundoWebC.cs
+++ Focus/Assets/Resources/Scripts/FundoWebC.cs
@@ -6,29 +6,62 @@ using UnityEngine.UI;
 public class FundoWebC : MonoBehaviour
 {
 
-	public Texture fundoOriginal;
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    [SerializeField] private RawImage background;
+    [SerializeField] private AspectRatioFitter fit;
+
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("No camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if(!devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if (backCam == null)
+        {
+            Debug.Log("Unable to find back camera");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvailable = true;
+    }
+
+    private void Update()
+    {
+        if (!camAvailable)
+        {
+            return;
+        }
+
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        float scaley = backCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaley, 1f);
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
 
 
-	private RawImage raw;
-
-	// Use this for initialization
-	void Start ()
-	{		        
-		raw = GetComponent<RawImage> ();
-		WebCamTexture webcamTexture = new WebCamTexture();
-
-		if (WebCamTexture.devices.Length > 0 && raw !=null) {
-			raw.texture = webcamTexture;
-			//raw.material.mainTexture = webcamTexture;
-			webcamTexture.Play ();
-		}
-	}
-
-	
-	// Update is called once per frame
-	void Update ()
-	{
-
-	}
+    }
 }
 
