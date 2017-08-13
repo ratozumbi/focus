a6fd5054482e80191eb2232aded66b7654bcc736
Ruilan Berg <rui_berg@hotmail.com>
2017-08-13 13:18:04 -0300

    add triangle light in scene and add behaviour

--- Focus/Assets/Resources/Scripts/Ruilan/LightBehaviour.cs
+++ Focus/Assets/Resources/Scripts/Ruilan/LightBehaviour.cs
@@ -5,48 +5,25 @@ using UnityEngine.UI;
 
 public class LightBehaviour : MonoBehaviour {
 
-    [SerializeField] private Slider focusPosition;
+    [SerializeField] private Transform focusPosition;
+    [SerializeField] private Transform playerPosition;
 
-    private RectTransform imglight;
-
-    private float rotZMin = -50;
-    private float zoomMax = -150;
-    private float zoommin = 0;
-
+    [Space]
+    [SerializeField] private Transform baseLight;
+    [SerializeField] private Transform topLight;
+    
 	// Use this for initialization
 	void Start () {
-        imglight = GetComponent<RectTransform>();
+        //InvokeRepeating("UpdatePosition", 2, 0.1f);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        CalculateRot();
-        Calculateheight();
-    }
-
-    private void CalculateRot()
+    private void Update()
     {
-        float posFocus = focusPosition.value;
-        imglight.rotation = Quaternion.Euler(0, 0, -(rotZMin + (100 * posFocus)));
-    }
+        if (focusPosition && playerPosition && baseLight && topLight)
+        {
+            baseLight.position = new Vector3(playerPosition.position.x, baseLight.position.y);
+            topLight.position = new Vector3(focusPosition.position.x, topLight.position.y);
 
-    private void Calculateheight()
-    {
-        float posFocus = focusPosition.value;
-        float valDisplaced;
-
-        if (posFocus > 0.5)
-            valDisplaced = posFocus;
-        else if (posFocus < 0.5)
-            valDisplaced = posFocus + 1;
-        else
-            valDisplaced = 0;
-
-        float rotImg = (imglight.rotation.z < 0) ? (imglight.rotation.z * -1) : imglight.rotation.z;
-        float valTop = ((rotImg * 3.5f) * valDisplaced) * 200;
-
-        imglight.offsetMax = new Vector2(imglight.offsetMax.x, valTop);
-
-        Debug.Log(valDisplaced + " - " + ((imglight.rotation.z * 3.5f) * valDisplaced) * 200);
+        }
     }
 }
