f240327ae3cdd95967514d0bbc5f1b180ac68537
Ruilan Berg <rui_berg@hotmail.com>
2017-08-17 11:32:17 -0300

    add force magnetic

---
+++ Focus/Assets/Resources/Scripts/Ruilan/Magnetic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnetic : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float forceGravity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if(Vector2.Distance(player.position, this.transform.position) < 1.5) {
+            Vector2 direction = this.transform.position - player.position;
+            direction = direction.normalized;
+            player.Translate(direction * forceGravity, Space.World);
+        }
+    }
+
+}
