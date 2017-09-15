74a7012627a74aa447e299e518ace61c90d822d6
Ruilan Berg <rui_berg@hotmail.com>
2017-09-15 18:05:33 -0300

    camera follow smoth player

---
+++ Focus/Assets/Resources/Scripts/Ruilan/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] private float interpVelocity;
+    [SerializeField] private float minDistance;
+    [SerializeField] private float followDistance;
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float velocity;
+    Vector3 targetPos;
+    // Use this for initialization
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
+
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+
+            interpVelocity = targetDirection.magnitude * velocity;
+
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+        }
+    }
+}
