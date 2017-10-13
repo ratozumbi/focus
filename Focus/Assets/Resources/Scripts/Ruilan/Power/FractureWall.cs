d63d2a50a940c6b4a5a0f1345e5203abaf6ddf77
RUILANBERG <rui_berg@hotmail.com>
2017-10-13 17:03:56 -0300

    fracture wall power

---
+++ Focus/Assets/Resources/Scripts/Ruilan/Power/FractureWall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureWall : MonoBehaviour {
+
+    [SerializeField] GameObject wallDestroy;
+
+    private Collider2D coll;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            for (int i = 0; i < Inventory.instance.itemSlot.Length; i++)
+            {
+                if (!Inventory.instance.itemSlot[i].IsEmpty && Inventory.instance.itemSlot[i].ItemInSlot.power == Power.FractureWalls)
+                {
+                    Destroy(wallDestroy);
+                    coll.enabled = false;
+                    break;
+                }
+            }
+
+        }
+    }
+}
