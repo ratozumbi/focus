710f7e5bf37d76c650f4893e6c0081fe370d9c5b
RUILANBERG <rui_berg@hotmail.com>
2017-10-13 23:38:56 -0300

    add score in plant sensory

---
+++ Focus/Assets/Resources/Scripts/Ruilan/PlantSensory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantSensory : MonoBehaviour {
+
+    [SerializeField] private TypeBubbleSensory typeSensory;
+    [SerializeField] private int scoreEarned;
+
+    private bool actived;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !actived)
+        {
+            SensoryHUD.instance.AddScore(scoreEarned, typeSensory);
+            actived = true;
+        }
+    }
+}
