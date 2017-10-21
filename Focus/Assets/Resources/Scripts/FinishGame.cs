f9c2e1f1c0b11d634fac6244107357d797de18c6
RUILANBERG <rui_berg@hotmail.com>
2017-10-21 21:30:28 -0200

    finish game afect blue

--- Focus/Assets/Resources/Scripts/FinishGame.cs
+++ Focus/Assets/Resources/Scripts/FinishGame.cs
@@ -8,6 +8,9 @@ public class FinishGame : MonoBehaviour
 	[SerializeField] private GameObject image;
 	[SerializeField] private GameObject centro;
 
+    [Space]
+    [SerializeField] private GameObject imgAudicao;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -27,20 +30,24 @@ public class FinishGame : MonoBehaviour
         }
     }
 
-	void Update(){
-		
-		int numEquipment = 0;
-		for (int i = 0; i < Inventory.instance.equipmentsSlots.Length; i++)
-		{
-			if(Inventory.instance.equipmentsSlots[i].GetEquipment != null)
-			{
-				++numEquipment;
-			}
-		}
-		if(numEquipment == (Inventory.instance.equipmentsSlots.Length))
-		{
-			centro.SetActive (false);
-		}
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player")
+        {
+            int numEquipment = 0;
+            for (int i = 0; i < Inventory.instance.equipmentsSlots.Length; i++)
+            {
+                if (Inventory.instance.equipmentsSlots[i].GetEquipment != null)
+                {
+                    ++numEquipment;
+                }
+            }
+            if (numEquipment == (Inventory.instance.equipmentsSlots.Length))
+            {
+                centro.SetActive(false);
+                imgAudicao.SetActive(true);
+
+            }
+        }
 	}
 
 
