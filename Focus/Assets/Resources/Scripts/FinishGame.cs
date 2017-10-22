8969283733626a2e7949d85f3a9a60ba5b9bf7ec
RUILANBERG <rui_berg@hotmail.com>
2017-10-21 22:05:40 -0200

    ajust finish game

--- Focus/Assets/Resources/Scripts/FinishGame.cs
+++ Focus/Assets/Resources/Scripts/FinishGame.cs
@@ -13,7 +13,8 @@ public class FinishGame : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        Debug.Log(Inventory.instance.equipmentsSlots.Length);
+        if (collision.gameObject.tag == "Player")
         {
             int numEquipment = 0;
             for (int i = 0; i < Inventory.instance.equipmentsSlots.Length; i++)
@@ -23,37 +24,18 @@ public class FinishGame : MonoBehaviour
                     ++numEquipment;
                 }
             }
-            if(numEquipment == (Inventory.instance.equipmentsSlots.Length - 1))
+            if(numEquipment == Inventory.instance.equipmentsSlots.Length)
             {
                 ResetScene();
             }
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Player")
-        {
-            int numEquipment = 0;
-            for (int i = 0; i < Inventory.instance.equipmentsSlots.Length; i++)
-            {
-                if (Inventory.instance.equipmentsSlots[i].GetEquipment != null)
-                {
-                    ++numEquipment;
-                }
-            }
-            if (numEquipment == (Inventory.instance.equipmentsSlots.Length))
-            {
-                centro.SetActive(false);
-                imgAudicao.SetActive(true);
-
-            }
-        }
-	}
-
-
     private void ResetScene()
     {
         image.SetActive(true);
+        centro.SetActive(false);
+        imgAudicao.SetActive(true);
         Invoke("LoadScene", 3f);
     }
 
