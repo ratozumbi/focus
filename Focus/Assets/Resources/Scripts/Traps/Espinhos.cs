d3823eaa12a1b62240e1645730ce1d3048f2dad8
Bruno Willian Andrade <brunowillianandrade@gmail.com>
2017-11-04 05:11:56 -0200

    espinhos done

---
+++ Focus/Assets/Resources/Scripts/Traps/Espinhos.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Espinhos : MonoBehaviour
+{
+
+	[SerializeField] private GameObject objTrap;
+	[SerializeField] private GameObject player;
+
+	[SerializeField] private int qtdToGen = 8;
+
+	[SerializeField] private int delay = 0;
+	[SerializeField] private int radius = 4;
+
+	[SerializeField] private int duration = 6;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Player");
+		StartCoroutine(doGen());
+	
+	}
+
+	IEnumerator doGen(){
+		
+		for (int i = 0; i < qtdToGen; i++) {
+
+			Vector3 objPos;
+			objPos = Random.insideUnitCircle * radius;
+
+			//while (objPos.x < 1 || objPos.y < 1) {
+			//	objPos = Random.insideUnitCircle * 4;
+			//}
+
+			objPos += player.transform.position;
+
+			GameObject espinho = Instantiate(objTrap, objPos, Quaternion.identity);
+
+			Destroy (espinho, duration);
+		
+			yield return new WaitForSeconds(delay);
+
+		}
+
+
+		Destroy (this.gameObject);
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
